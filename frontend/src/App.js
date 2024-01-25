7597a4b860301d072b01c812b8604fb08dd03933
gregorspan <gregor.span@student.um.si>
2024-01-25 22:04:52 +0100

    Routing Frontend WW- 22 DONE Time 1h

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Appbar from './Components/Appbar';
 import Kategorija from './Components/Kategorija';
 import Uporabnik from './Components/Uporabnik';
+import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <Appbar />
-      <Kategorija />
-      <Uporabnik />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Appbar />
+        <Routes>
+          <Route path="/kategorija" element={<Kategorija />} />
+          <Route path="/uporabnik" element={<Uporabnik />} />
+          <Route
+            path="/"
+            element={
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <Link
+                  className="App-link"
+                  to="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </Link>
+              </header>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/Components/Appbar.js
+++ frontend/src/Components/Appbar.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
@@ -24,7 +24,12 @@ export default function Appbar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             WiseWallet
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Link to="/kategorija" style={{ color: 'white', textDecoration: 'none' }}>
+            Kategorija
+          </Link>
+          <Link to="/uporabnik" style={{ color: 'white', textDecoration: 'none', marginLeft: '10px' }}>
+            Uporabnik
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
